480b1887d84114b5a7707c1b1d438e142e80f361
Salvador Faria <salvador.mrf@gmail.com>
2015-01-06 21:57:18 +0000

    Adds travis-ci support

--- deployment/provisioners/install.sh
+++ deployment/provisioners/install.sh
@@ -7,6 +7,6 @@ virtualenv /tmp/jenkinsbox
 /tmp/jenkinsbox/bin/pip install -r deploy-requirements.txt
 
 # deploy to a vagrant
-/tmp/jenkinsbox/bin/ansible-galaxy install -r role-requirements.yml --ignore-errors
+/tmp/jenkinsbox/bin/ansible-galaxy install -r role-requirements.yml
 /tmp/jenkinsbox/bin/ansible-playbook -i inventory/vagrant playbooks/jenkins.yml  -vvvv
 
