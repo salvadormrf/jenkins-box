b6d9d78696a78f9e3b3b04a1ca25e9bdcd9b326f
Salvador Faria <salvador@bynd.com>
2015-01-07 16:37:24 +0000

    Adds email template

--- deployment/provisioners/install.sh
+++ deployment/provisioners/install.sh
@@ -4,10 +4,10 @@
 virtualenv /tmp/jenkinsbox
 /tmp/jenkinsbox/bin/pip install -r ansible/deploy-requirements.txt
 
-
+# run ansible from ansible folder
 cd ansible
 
 # deploy to a vagrant
 /tmp/jenkinsbox/bin/ansible-galaxy install -r role-requirements.yml
-/tmp/jenkinsbox/bin/ansible-playbook -i inventory/vagrant playbooks/jenkins.yml  -vvvv
+/tmp/jenkinsbox/bin/ansible-playbook -i inventory/vagrant playbooks/jenkins.yml -vvvv
 
